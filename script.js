5385bdcbdaf8b0bf89ab639e65108b88ec71ee40
xintamosaik <xintamosaik@gmail.com>
2022-06-09 12:09:26 +0200

    new module for userData

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+import { data, userData, registerNewUser, getUserInfo  } from './src/js/get-userdata.mjs';
+console.log(data.data.token)
+getUserInfo(data.data.token)
 const serverStatus = document.getElementById('serverStatus');
 const statusURL = "https://api.spacetraders.io/game/status"
+
 
 async function retriveServerStatus(){
     await fetch(statusURL,{cache:"no-cache"})
@@ -13,4 +17,29 @@ async function retriveServerStatus(){
                 serverStatus.innerHTML = jsonResponse['status'];
             });
 }
+const retriveServerStatus2 = async () => {
+    fetch("https://v2-0-0.alpha.spacetraders.io/", {
+        "method": "GET",
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            console.log(response);
+            return response.json()
+        })
+        .then(jsonData => {
+            console.log(jsonData)
+        })
+        .catch(err => {
+            console.error(err);
+        })
+}
 
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    //retriveServerStatus2() <-- not needed rn but works
+
+})
