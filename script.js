a032c655a167812dd56f230d70d713847b682f59
xintamosaik <xintamosaik@gmail.com>
2022-06-19 23:32:23 +0200

    update UI for user register

--- script.js
+++ script.js
@@ -7,13 +7,41 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
     //retriveServerStatus2() <-- not needed rn but works
     
-    // THIS FETCHES THE NEEDED DATA:
-    //getUserInfo(userData.data.token)
 
     // THIS REGISTERS A NEW USER
-    // registerNewUser("username")
+    // 
 
+    const registerButton = document.getElementById("registerUser")
+    registerButton.style.color = "green"
+    const userInput = document.getElementById("userAccount")
+
+    registerButton.addEventListener("click", event => {
+      
+        const userName = userInput.value
+        // This should be a promise to wait for the answer
+        // and put 
+        registerNewUser(userName)
+
+    } )
+    
+        // THIS FETCHES THE NEEDED DATA:
+    const userDataButton = document.getElementById("userStatusButton")
+    userDataButton.style.color = "red"
+    const userDataResponse = document.getElementById("userDataResponse")
+
+    userDataButton.addEventListener("click", event => {
+      
+        
+        // This should be a promise to wait for the answer
+        // and put 
+        getUserInfo(userData.data.token, userDataResponse)
+
+    } )
+   
+    
+
+    
     // THIS HOLDS CRITICAL DATA.. NOT SURE HOW TO HANDLE IT
-    // console.log(userData)
+    console.log(userData.data.agent.symbol)
 
 })
