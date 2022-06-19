f6c001fd87a23664e6dfefc572f704d8fa530704
xintamosaik <xintamosaik@gmail.com>
2022-06-19 23:32:34 +0200

    Also this

--- src/js/get-userdata.mjs
+++ src/js/get-userdata.mjs
@@ -1,9 +1,10 @@
 const registerNewUser = async user => {
-    
+    const registerOutput = document.getElementById("registerResponse")
     const data = JSON.parse(localStorage.getItem("userData"))
     
     if (data.data.token !== undefined) {
         console.log("User is already registered in local space")
+        registerOutput.innerHTML = "User is already registered in local space"
         return
     }  
 
@@ -21,7 +22,7 @@ const registerNewUser = async user => {
         })
         .then(jsonData => {
             localStorage.setItem("userData", JSON.stringify(jsonData));
-            
+            registerOutput.innerHTML = jsonData.data.token
         })
         .catch(err => {
             console.error(err);
@@ -35,9 +36,10 @@ if (userData.data.token !== undefined) {
     console.log("User is already registered in local space")
 }
 
-const getUserInfo = token => {
+const getUserInfo = (token, responseOutput) => {
     if (userData.data.token === undefined) {
         console.log("User is not registered")
+        responseOutput.innerHTML = "User is not registered"
         return
     }
     fetch("https://v2-0-0.alpha.spacetraders.io/my/agent", {
@@ -53,7 +55,8 @@ const getUserInfo = token => {
             return response.json()
         })
         .then(jsonData => {
-            console.log(jsonData)
+            console.log(jsonData.data.symbol)
+            responseOutput.innerHTML = jsonData.data.symbol
         })
         .catch(err => {
             console.error(err);
