625f3ff6fe0398c2b396bdc2610adc367c4f8792
gcnw <SkillCatcher@pm.me>
2022-07-04 20:29:47 -0700

    updated SpaceTradersGet to a general fetch that will accept a body parameter

--- src/js/list-markets.js
+++ src/js/list-markets.js
@@ -1,4 +1,4 @@
-import { SpaceTradersGet } from "./spaceTradersGet.mjs"
+import { SpaceTradersFetch } from "./SpaceTradersFetch.mjs"
 
 //Placeholder - Add selection ability later
 let systemSymbol = "X1-OE" 
@@ -7,7 +7,7 @@ let systemSymbol = "X1-OE"
 const marketListURL = `https://v2-0-0.alpha.spacetraders.io/systems/${ systemSymbol }/markets`;
 const marketListContainer = document.getElementById('list-markets-response');
 
-const listMarketsCall = new SpaceTradersGet(marketListURL, marketListContainer);
+const listMarketsCall = new SpaceTradersFetch(marketListURL, marketListContainer);
 document.getElementById('list-markets-button').addEventListener('click', () => listMarketsCall.fetchData());
 
 
